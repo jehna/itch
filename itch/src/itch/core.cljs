0af9c7692b61ac7b59226d4a4fc4f491a8cfa783
Jesse Luoto <jesse.luoto@gmail.com>
2020-03-09 23:26:38 +0200

    Add coordinates to AST

--- itch/src/itch/core.cljs
+++ itch/src/itch/core.cljs
@@ -18,10 +18,20 @@
   (dissoc ast :env))
 (def st (cljs/empty-state))
 (def ast (atom nil))
+
+(defn add-coords [value]
+  (->> value
+       (merge {:x 0 :y 0})
+       (l/over (l/key :args) #(map add-coords %))
+       ))
+
+(defn add-coords-root [root]
+  (l/over (l/key :value) add-coords root))
+
 (cljs/analyze-str st "(+ 1 (+ 2 3))" nil
                   {:passes [ana/infer-type elide-env]}
                   (fn [{:keys [error value] :as res}]
-                    (reset! ast res)))
+                    (reset! ast (add-coords-root res))))
 (defn all-items [ast] (flatten [ast (map all-items (:args ast))]))
 
 (defn get-name [ast]
@@ -32,23 +42,43 @@
 
 (defonce app-state (atom {:text "Hello world!"}))
 
-(defn nest-lenses [ast]
-  [ast
+(defn nest-lens-nodes [ast]
+  (concat
+   [ast]
    (map-indexed
-    #(nest-lenses (l/derive (comp (l/key :args) (l/nth %)) ast)) (:args @ast))])
+    #(nest-lens-nodes (l/derive (comp (l/key :args) (l/nth %)) ast)) (:args @ast))))
+
+(defn nodes-to-connections
+  [[from & tos]]
+  (flatten
+   (concat
+    (map (fn [[to]] {:from from :to to}) tos)
+    (map nodes-to-connections tos))))
 
 (defn node-renderer
-  [i ast]
-  [:<> {:key i}
-   [:rect {:width 100 :height 100 :x (* i 110) :stroke "#000" :fill "none"}]
-   [:text {:x (+ 35 (* i 110)) :y 65 :style {:font-size 50}} (get-name @ast)]])
+  [ast]
+  [:<>
+   [:rect {:width 100 :height 100 :x (:x @ast) :y (:y @ast) :stroke "#000" :fill "none"}]
+   [:text {:x (+ 35 (:x @ast)) :y (+ 65 (:y @ast)) :style {:font-size 50}} (get-name @ast)]])
+
+(defn connection-renderer
+  [in]
+  (let [from (:from in)
+        to (:to in)]
+       
+       [:line {:x1 (:x @from)
+               :x2 (:x @to)
+               :y1 (:y @from)
+               :y2 (:y @to)}]))
 
 (defn hello-world []
   [:div
    #_[:pre (with-out-str (cljs.pprint/pprint (all-items (:value @ast))))]
    [:pre (with-out-str (cljs.pprint/pprint @ast))]
    [:svg {:viewBox "0 0 1024 762" :width 1024 :height 762 :xmlns "http://www.w3.org/2000/svg"}
-    (map-indexed node-renderer (flatten (nest-lenses (l/derive (l/key :value) ast))))]])
+    (map node-renderer (flatten (nest-lens-nodes (l/derive (l/key :value) ast))))
+    (map connection-renderer (nodes-to-connections (nest-lens-nodes (l/derive (l/key :value) ast))))
+    ]])
 
 (reagent/render-component [hello-world]
                           (. js/document (getElementById "app")))
